789c2f2de137c6f22e3ee3d3a6e8496e165c8d6a
TuranSadigli <84472232+TuranSadigli-1337@users.noreply.github.com>
2026-04-25 23:20:42 +0400

    Override and Processing Tasks in Hydroponics

--- src/main/java/processors/Hydroponics.java
+++ src/main/java/processors/Hydroponics.java
@@ -1,5 +1,16 @@
 package processors;
 
-public class Hydroponics {
+import tasks.ColonyTask;
 
+public class Hydroponics implements IProcessor {
+    @Override
+    public boolean canProcess(ColonyTask task) {
+        return task.getTaskType().equals("LIFE_SUPPORT") ||
+                task.getTaskType().equals("RESEARCH_TASK");
+    }
+
+    @Override
+    public String processTask(ColonyTask task) {
+        return "🌱 Hydroponics completed: " + task.getName();
+    }
 }
