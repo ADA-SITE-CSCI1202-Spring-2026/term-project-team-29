9327cf0eda22520c3010474f9074fd312eecb919
TuranSadigli <84472232+TuranSadigli-1337@users.noreply.github.com>
2026-04-25 23:21:24 +0400

    ProcessTask and Override in MedicalWard

--- src/main/java/processors/MedicalWard.java
+++ src/main/java/processors/MedicalWard.java
@@ -1,5 +1,15 @@
 package processors;
 
-public class MedicalWard {
+import tasks.ColonyTask;
 
+public class MedicalWard implements IProcessor {
+    @Override
+    public boolean canProcess(ColonyTask task) {
+        return task.getTaskType().equals("LIFE_SUPPORT");
+    }
+
+    @Override
+    public String processTask(ColonyTask task) {
+        return "🏥 Medical Ward completed: " + task.getName();
+    }
 }
